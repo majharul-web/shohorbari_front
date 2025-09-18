86ec4a3d0eab94ed192582789cedec5c4451959a
majharul <majharuljony163@gmail.com>
2025-09-18 08:59:03 +0600

    fix: deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -13,5 +13,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
-  base: "/", // 👈 ensures assets (like /login.png) load correctly
+  base: "/",
 });
