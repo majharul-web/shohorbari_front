3e000422fc803a88c5fa3cf89ce6a84d8e4e8de0
majharul <majharuljony163@gmail.com>
2025-09-18 08:58:25 +0600

    fix:vercel error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  },
+  base: "/", // 👈 ensures assets (like /login.png) load correctly
 });
